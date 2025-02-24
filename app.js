4d2ecaee1da128790fb6aaecf5ea9213f9a8d723
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-02-23 22:53:00 -0300

    Implementar função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome == '') {
+        console.log(nome);
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(nome);
+        console.log(amigos);
+    }
+
+    limparCampo();
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
